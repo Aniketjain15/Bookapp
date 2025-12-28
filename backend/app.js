8b731cf354542d27383cbe1fa8ef4afd790716f7
ANIKET JAIN <aniket7247213350@gmail.com>
2025-12-28 22:15:28 +0530

    Update CORS origin for Netlify frontend

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 1000;
 // Allow only your Netlify frontend
 app.use(
   cors({
-    origin: "https://bookverse-47wi.onrender.com",
+    origin: "https://bookapp-d1oh.onrender.com",
     credentials: true,
   })
 );
