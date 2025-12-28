7bb19c93d2be7eb2b29272bf1429f72eee50856b
ANIKET JAIN <aniket7247213350@gmail.com>
2025-12-28 22:55:11 +0530

    Refactor order schema and update model export

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,23 +1,21 @@
 const mongoose = require("mongoose");
-const orderSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const order = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "user",
+    },
+    book: {
+      type: mongoose.Types.ObjectId,
+      ref: "books",
+    },
+    status: {
+      type: String,
+      default: "Order placed",
+      enum: ["Order placed", "Out for delivery", "Delivered", "Canceled"],
+    },
   },
-  book: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Book",
-    required: true,
-  },
-  paymentId: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model("order", order);
